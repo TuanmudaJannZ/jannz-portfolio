493fcae3a9ce8125218e5633dabd6831c57a3929
AHMAD JANUAR D.K <ahmadjanuardk@gmail.com>
2026-02-03 08:32:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jannz-portfolio/',
+  base: 'https://portofolio.jannz.my.id/',  // ⭐ HARUS '/' untuk custom domain
 })
